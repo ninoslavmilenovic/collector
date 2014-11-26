57bb8125cb96be83f928098ccf5cf63918306c16
pythogorian <nino.mil@gmail.com>
2014-11-27 00:41:51 +0100

    Update syntax

--- app/models/chunk.rb
+++ app/models/chunk.rb
@@ -2,5 +2,5 @@ class Chunk < ActiveRecord::Base
   belongs_to :user
 
   validates :content, :public_ip, :private_ip, presence: true
-  validates :public_ip, :private_ip, format: { :with => Resolv::IPv4::Regex }
+  validates :public_ip, :private_ip, format: { with: Resolv::IPv4::Regex }
 end
