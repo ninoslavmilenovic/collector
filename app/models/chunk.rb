56761fc5f817fd1dc7fb7cf4b4303594079c4e86
pythogorian <nino.mil@gmail.com>
2014-11-26 03:29:18 +0100

    Add validations

--- app/models/chunk.rb
+++ app/models/chunk.rb
@@ -1,3 +1,6 @@
 class Chunk < ActiveRecord::Base
   belongs_to :user
+
+  validates :content, :public_ip, :private_ip, presence: true
+  validates :public_ip, :private_ip, format: { :with => Resolv::IPv4::Regex }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :chunks
-  before_save :ensure_authentication_token
+  
+  before_validation :ensure_authentication_token
+
+  validates :authentication_token, presence: true, uniqueness: true
 
   def ensure_authentication_token
     self.authentication_token ||= generate_authentication_token
