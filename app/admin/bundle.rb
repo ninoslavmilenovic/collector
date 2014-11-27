ae510f73870a76278bd87137468be57c11b8665e
pythogorian <nino.mil@gmail.com>
2014-11-27 02:03:45 +0100

    Fix dates

--- app/admin/bundle.rb
+++ app/admin/bundle.rb
@@ -1,15 +1,14 @@
 ActiveAdmin.register_page 'Bundle' do
   sidebar :daily_bundles do
     ul do
-      Bundle.last(30).each do |bundle|
+      Bundle.last(30).reverse.each do |bundle|
         li link_to(bundle.date.strftime("%d %B, %Y"), admin_bundle_path(date: bundle.date))
       end
     end
   end
 
   content do
-    date = Date.parse(params[:date]) rescue (proc { Date.today }).call
-
+    date = Date.parse(params[:date]) rescue Bundle.last(1).last.date
     h3 date.strftime("%d %B, %Y")
     para simple_format(Bundle.get(date).content)
   end
